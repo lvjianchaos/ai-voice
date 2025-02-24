f948163635fbe8ce2188f83a3b01a0ad28e3b2b9
lvjianchaos <2277146577@qq.com>
2025-02-24 20:43:09 +0800

    use card and table to realize SampleIndex.vue

--- frontend/src/views/sample/SampleIndex.vue
+++ frontend/src/views/sample/SampleIndex.vue
@@ -2,7 +2,7 @@
 import { getAll } from '@/api/sound'
 import type { SoundItem } from '@/api/sound'
 // 存储返回数据
-const allSounds = ref([] as SoundItem[])
+const allSounds = ref([{id:1,name:"Chaos",discription:"用户Chaos的声音"}] as SoundItem[])
 // 获取所有声音
 const getAllSounds = async() => {
   const { data } = await getAll()
@@ -18,8 +18,40 @@ getAllSounds()
 </script>
 
 <template>
-  <h2>声音样本库</h2>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <el-button type="primary">添加菜单</el-button>
+      </div>
+    </template>
+    <el-table :data="allSounds" border style="width: 100%">
+      <el-table-column type="index" label="序号" align="center" />
+      <el-table-column prop="name" label="样本名称" align="center"/>
+      <el-table-column prop="discription" label="样本描述" align="center"/>
+      <el-table-column label="操作" align="center">
+        <el-button type="primary">编辑</el-button>
+        <el-button type="danger">删除</el-button>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <style lang="scss" scoped>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: auto;
+}
 </style>
